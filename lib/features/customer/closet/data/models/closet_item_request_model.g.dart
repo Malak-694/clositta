18614c7e4e53fd7ce25c61932d444ffb9d7786d7
Rawan2Amr <147277145+Rawan2Amr@users.noreply.github.com>
2026-03-04 22:03:11 +0200

    add closet model

---
+++ lib/features/customer/closet/data/models/closet_item_request_model.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'closet_item_request_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ClosetItemRequestModel _$ClosetItemRequestModelFromJson(
+  Map<String, dynamic> json,
+) => ClosetItemRequestModel(
+  image: json['image'] as String?,
+  name: json['name'] as String,
+  category: json['category'] as String,
+  season: json['season'] as String,
+  color: json['color'] as String,
+);
+
+Map<String, dynamic> _$ClosetItemRequestModelToJson(
+  ClosetItemRequestModel instance,
+) => <String, dynamic>{
+  'image': instance.image,
+  'name': instance.name,
+  'category': instance.category,
+  'season': instance.season,
+  'color': instance.color,
+};
